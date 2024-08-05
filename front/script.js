ecde63883364d4763e16af4ea6c4515d7daf1218
Erick Giraldo <erickgiraldo94@gmail.com>
2024-08-05 12:38:36 -0500

    Added front to test login

--- front/script.js
+++ front/script.js
@@ -1,29 +1,67 @@
+const login = () => {
 
-fetch('http://localhost:3000/api/users/', {
-    method: "POST",
-    headers: {
-        "Content-Type":"application/json",
-        // "Authorization": true
-    },
-    body:JSON.stringify({
-        name: "jose bareto",
-        email: "jose@bareto.com",
-        password: "123"
-    })
-}).then(response => {
-    console.log(response.json())
-}).catch(()=>{
-    console.log("Error fetching")
-})
+    const root = document.getElementById('root');
 
-// const testFunc = async () =>{
-//     try{
-//         const response = await fetch('http://localhost:3000/api/users/');
-//         console.log("response: ", response)
-//     }catch(error){
-//         console.log("error: ", error)
-//     }
-// }
+    const loginHtml = `
+        <div>
+        <div id="login-form-container">
+            <div id="login-container">
+                <input class="name-login" type="text" name="email" placeholder="email@example.com" />
+                <input class="password-login" type="password" name="password" placeholder="password" />
+                <button class="submit-button" id="submit">Login</button>
+                <button class="register-button" id="register" >Register</button>
+            </div>
+        </div>
+        <div id="token-container">
+        </div>
+        <div>
+    `;
 
-// testFunc()
-// console.log("response: ", response)
+    root.innerHTML = "";
+    root.innerHTML = loginHtml;
+
+    // const btnRegister = document.getElementById('register');
+    // btnRegister.addEventListener('click', () => {
+    //     register(array)
+    // });
+
+    const $submit = document.getElementById('submit');
+    $submit.addEventListener('click', (e) => {
+        e.preventDefault();
+        const email = document.querySelector('input[name="email"]').value;
+        const password = document.querySelector('input[name="password"]').value;
+        
+        fetch('http://localhost:3000/api/auth/login', {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+                // "Authorization": true
+            },
+            body:JSON.stringify({
+                email,
+                password
+            })
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        }).then(data => {
+            console.log("Login successful!", data);
+            const $tokenContainer = document.getElementById('token-container');
+            $tokenContainer.innerHTML = `
+                <h2>Log in succesful</h2>
+                <p>Here is your token: ${data.token}</p>
+            `
+        }).catch(()=>{
+            console.log("Error fetching")
+            const $tokenContainer = document.getElementById('token-container');
+            $tokenContainer.innerHTML = `
+                <h2>Email or password incorrect</h2>
+            `
+        })
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', login)
--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,5 @@
 export const corsOption = {
-    origin: "http://127.0.0.1:5500",
+    origin: ["http://127.0.0.1:5500", "http://127.0.0.1:5501"],
     methods: "GET, POST",
     allowHeaders: ['Content-Type', 'Authorization'],
     credentials: true
